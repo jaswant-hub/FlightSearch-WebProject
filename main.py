933ea6af3eaabbdc31a1c9606976d545a2b13f4e
Jaswant Reddy <reddyjaswant0199@gmail.com>
2022-12-07 22:06:16 +0530

    Modified commit

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv("APP_SECRET_KEY")
 Bootstrap(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///flight-db.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "sqlite:///flight-db.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
